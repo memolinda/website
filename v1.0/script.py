088b46660c4bbd5513774f063ea7c52d03e4a592
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-29 11:40:36 +0200

    v1.0 add html file template

--- v1.0/script.py
+++ v1.0/script.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/') #create a decorator for the home page
 
 def home(): #content in the decorator
-    return "Home contents goes here!"
+    return render_template("home.html")
 
 @app.route('/about/') #create an about page
 
